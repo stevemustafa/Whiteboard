985c6043c34619c5d91c146f6706e4ef7de4758f
Steve Mustafa <steven.i.mustafa@gmail.com>
2013-03-27 21:27:00 -0600

    created Rails Web app and Node folder

---
+++ Whiteboard_web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhiteboardWeb::Application.config.secret_token = 'b1fcd9f6b5be3cb3ced7a4ff962986c673ca8985e20a7687b2513a340a9f7b61ba5834e322d2adc33955c7ae7c291bea53615d831fbd866f8d8ca28913353d3e'
